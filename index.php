ba8f6afb13567c38a589cc1d5550f7a338c0bc52
Dsgathebest <gomezabrildaniel1234@gmail.com>
2023-05-25 09:32:22 -0500

    iniciando PHP con POO

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+    include_once './Model/persona.php';
+    $alumno = new Persona ('Daniel Gómez',23);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php include('includes/header.php') ?>
+    </header>
+</body>
+</html>
